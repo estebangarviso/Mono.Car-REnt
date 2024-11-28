a2ebe36a0e2a8d84f8302db2107c91f25dd097bf
Esteban Garviso <e.garvisovenegas@gmail.com>
2024-11-28 00:47:46 -0300

    chore: reorganize imported dependencies

--- src/main/java/com/mono_car_rent/vehicle/Vehicle.java
+++ src/main/java/com/mono_car_rent/vehicle/Vehicle.java
@@ -1,10 +1,10 @@
 package com.mono_car_rent.vehicle;
 
-import java.time.Year;
-
 import com.mono_car_rent.common.Service;
 import com.mono_car_rent.common.exception.general.BadRequestException;
 import com.mono_car_rent.vehicle.use_case.*;
+import java.time.Year;
+
 
 public class Vehicle extends Service implements ValidateLicensePlateUseCase, ValidateBrandUseCase, ValidateModelUseCase, ValidateManufactureYearUseCase {
     private String licensePlate;
@@ -85,18 +85,22 @@ public class Vehicle extends Service implements ValidateLicensePlateUseCase, Val
     //#endregion
 
     //#region Validation
+    @Override
     public boolean isValidLicensePlate(String licensePlate) {
         return licensePlate.matches("^[A-Z]{4}[0-9]{2}");
     }
 
+    @Override
     public boolean isValidBrand(String brand) {
         return brand.matches("^[A-Z0-9]+$");
     }
 
+    @Override
     public boolean isValidModel(String model) {
         return model.matches("^[A-Z0-9]+$");
     }
 
+    @Override
     public boolean isValidManufactureYear(int manufactureYear) {
         var currentYear = Year.now().getValue();
         return manufactureYear >= 2000 && manufactureYear <= currentYear;
