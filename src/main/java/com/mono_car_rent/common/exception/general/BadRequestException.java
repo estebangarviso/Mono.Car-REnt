2b0ef33e4e826f42b72f0fe1cca51c19259841a8
Esteban Garviso <e.garvisovenegas@gmail.com>
2024-12-09 23:34:30 -0300

    feat: enhance exception handling with new error codes and NotFoundException class

--- src/main/java/com/mono_car_rent/common/exception/general/BadRequestException.java
+++ src/main/java/com/mono_car_rent/common/exception/general/BadRequestException.java
@@ -13,7 +13,19 @@ public class BadRequestException extends AppException {
         super(code, cause);
     }
 
-    // Vehicle
+    //#region Common
+    public static BadRequestException invalidId() {
+        return new BadRequestException(AppExceptionCode.INVALID_ID, new Throwable("Invalid id"));
+    }
+    public static BadRequestException isRequired(String field) {
+        return new BadRequestException(AppExceptionCode.FIELD_IS_REQUIRED, new Throwable(field + " is required"));
+    }
+    public static Throwable invalidPage(int page, int totalPages) {
+        return new BadRequestException(AppExceptionCode.INVALID_PAGE, new Throwable("Invalid page, must be between 1 and " + totalPages));
+    }
+    //#endregion
+
+    //#region Vehicle
     public static BadRequestException invalidLicensePlate() {
         return new BadRequestException(AppExceptionCode.INVALID_LICENSE_PLATE, new Throwable("Invalid license plate, must be 8 characters and uppercase"));
     }
@@ -29,8 +41,8 @@ public class BadRequestException extends AppException {
     public static BadRequestException invalidManufactureYear() {
         return new BadRequestException(AppExceptionCode.INVALID_MANUFACTURER_YEAR, new Throwable("Invalid manufacturer year, must be between 2000 and current year"));
     }
-
-    // Customer
+    //#endregion
+    //#region Customer
     public static BadRequestException invalidIdentityCard() {
         return new BadRequestException(AppExceptionCode.INVALID_IDENTITY_CARD, new Throwable("Invalid identity card. Must be 10 characters long in total, penultimate must be a hyphen and last character must be a number between 0 and 9 or a letter k"));
     }
@@ -38,14 +50,22 @@ public class BadRequestException extends AppException {
     public static BadRequestException customerIsNotActive() {
         return new BadRequestException(AppExceptionCode.CUSTOMER_IS_NOT_ACTIVE, new Throwable("Customer is not active, cannot rent a vehicle"));
     }
-
-    // Rental
+    //#endregion
+    //#region Rental
     public static BadRequestException invalidRentalDays() {
         return new BadRequestException(AppExceptionCode.INVALID_RENTAL_DAYS, new Throwable("Invalid rental days, must be greater than 1 and less than 10"));
     }
-
-    // Rental Return
+    public static BadRequestException invalidRentalDate() {
+        return new BadRequestException(AppExceptionCode.INVALID_RENTAL_DATE, new Throwable("Invalid rental date, cannot be before today"));
+    }
+    //#endregion
+    //#region Rental Return
     public static BadRequestException invalidReturnDate() {
         return new BadRequestException(AppExceptionCode.INVALID_RETURN_DATE, new Throwable("Invalid return date, cannot be before the rental date"));
     }
+
+    public static Throwable customerHasRentals() {
+        return new BadRequestException(AppExceptionCode.CUSTOMER_HAS_RENTALS, new Throwable("Customer has rentals, cannot be deleted"));
+    }
+    //#endregion
 }
