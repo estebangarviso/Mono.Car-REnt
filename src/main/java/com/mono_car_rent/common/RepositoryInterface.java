c51a19dfba8ae927167b53bf2d99f512049d35bc
Esteban Garviso <e.garvisovenegas@gmail.com>
2024-12-09 23:34:58 -0300

    refactor: remove RepositoryInterface and Service classes to streamline codebase

--- src/main/java/com/mono_car_rent/common/RepositoryInterface.java
+++
@@ -1,8 +0,0 @@
-package com.mono_car_rent.common;
-
-public interface RepositoryInterface<T> {
-    public void save(T entity);
-    public void delete(int id);
-    public void update(T entity);
-    public T findById(int id);
-}
